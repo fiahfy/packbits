4f0854a6d16730a8916a6c2cf802214768abbe26
fiahfy <d.fiahfy@gmail.com>
2018-09-26 22:36:55 +0900

    Impl icns packbits decoding

--- src/index.js
+++ src/index.js
@@ -3,5 +3,29 @@ export const encode = (buf) => {
 }
 
 export const decode = (buf) => {
-  return buf
+  const bufs = []
+
+  let i = 0
+  while (i < buf.length) {
+    let num = buf.readUInt8(i)
+
+    let o
+    if (num >= 128) {
+      // num = 256 - num
+      // o = Buffer.alloc(num + 1, buf.slice(i + 1, i + 2))
+      num = num - 128
+      o = Buffer.alloc(num + 3, buf.slice(i + 1, i + 2))
+      i += 2
+    } else {
+      o = buf.slice(i + 1, i + num + 2)
+      i += num + 2
+    }
+
+    bufs.push(o)
+  }
+
+  const list = bufs
+  const totalLength = bufs.reduce((carry, buf) => carry + buf.length, 0)
+
+  return Buffer.concat(list, totalLength)
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,30 @@
+import { decode } from '../src'
+
+const hexToBuffer = (str) => {
+  const hex = str.split(' ')
+  const buf = Buffer.alloc(hex.length)
+  hex.forEach((hex, i) => {
+    buf.writeUInt8(parseInt(hex, 16), i)
+  })
+  return buf
+}
+
+const bufferToHex = (buf) => {
+  const str = []
+  let pos = 0
+  while (pos < buf.length) {
+    const hex = ('00' + buf.readUInt8(pos).toString(16)).slice(-2).toUpperCase()
+    str.push(hex)
+    pos++
+  }
+  return str.join(' ')
+}
+
 describe('rle', () => {
   test('should work', () => {
+    const buf = hexToBuffer('02 01 02 02 80 03 81 04 82 05')
+    const decoded = decode(buf)
+    const hex = bufferToHex(decoded)
+    expect(hex).toBe('01 02 02 03 03 03 04 04 04 04 05 05 05 05 05')
   })
 })
